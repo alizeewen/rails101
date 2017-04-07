2a22e924121315dd7b0b45b2ec7929f8258cb3d0
陈文娟 <chenwenjuan@chenwenjuandeMacBook-Pro.local>
2017-04-07 22:52:46 +0800

    style form with bootstrap

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -34,11 +34,6 @@ class GroupsController < ApplicationController
     end
    end
 
-     @group.update(group_params)
-
-     redirect_to groups_path, notice: "Update Success"
-   end
-
     def destroy
       @group = Group.find(params[:id])
       @group.destroy
